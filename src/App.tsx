36660814e02b0ac16590bf3b1ad28618ac260f47
Harsh Kumar Gupta <hrslsha007@gmail.com>
2026-08-27 17:44:34 +0530

    feat: implement evidence-based data architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import GodTierVersion from './versions/GodTierVersion'
+import EvidenceVersion from './versions/EvidenceVersion'
 import './styles/god-tier.css'
 import './styles/globals.css'
 
 function App() {
   return (
     <div className="relative">
-      <GodTierVersion />
+      <EvidenceVersion />
     </div>
   )
 }
